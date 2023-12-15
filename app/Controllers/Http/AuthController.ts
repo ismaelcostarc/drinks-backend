033f532095a97643f1255ed6ed6d9b1d983c4c16
Ismael Costa <ismaelac96@icloud.com>
2023-12-15 13:12:16 -0300

    feat: create auth controllers and api tokens migration

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,47 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
+import User from "App/Models/User";
+
+export default class AuthController {
+  public async login({ request, auth }: HttpContextContract) {
+    const newPostSchema = schema.create({
+      email: schema.string(),
+      password: schema.string(),
+    })
+
+    const payload = await request.validate({ schema: newPostSchema })
+
+    const email = payload.email
+    const password = payload.password
+
+    const token = await auth.use("api").attempt(email, password, {
+      expiresIn: "10 days",
+    });
+
+    return token.toJSON();
+  }
+
+  public async register({ request, auth }: HttpContextContract) {
+    const newPostSchema = schema.create({
+      email: schema.string(),
+      password: schema.string(),
+    })
+
+    const payload = await request.validate({ schema: newPostSchema })
+
+    const email = payload.email
+    const password = payload.password
+
+    const user = new User();
+    user.email = email;
+    user.password = password;
+
+    await user.save();
+
+    const token = await auth.use("api").login(user, {
+      expiresIn: "10 days",
+    });
+
+    return token.toJSON();
+  }
+}
