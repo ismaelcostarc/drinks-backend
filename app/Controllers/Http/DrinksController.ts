367281c1d66184e5b8ea9630807db2fe831256b8
Ismael Costa <ismaelac96@icloud.com>
2023-12-15 14:47:38 -0300

    feat: create drinks model, controller and migration

---
+++ app/Controllers/Http/DrinksController.ts
@@ -0,0 +1,60 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Drink from 'App/Models/Drink';
+
+export default class DrinksController {
+  public async index({ request }: HttpContextContract) {
+    const drinks = await Drink.query();
+    return drinks
+  }
+
+  public async show({ request, params }: HttpContextContract) {
+    try {
+      const drink = await Drink.find(params.id);
+      if (drink) {
+        return drink
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public async getByCategory({ params, response }: HttpContextContract) {
+    const { categoryId } = params;
+
+    try {
+      const drinks = await Drink.query().where('category_id', categoryId);
+      return response.ok(drinks);
+    } catch (error) {
+      return response.badRequest({ message: 'Erro ao buscar as bebidas da categoria.' });
+    }
+  }
+
+  public async update({ auth, request, params }: HttpContextContract) {
+    const drink = await Drink.find(params.id);
+    if (drink) {
+      drink.name = request.input('name');
+      drink.description = request.input('description');
+
+      if (await drink.save()) {
+        return drink
+      }
+      return; // 422
+    }
+    return; // 401
+  }
+
+  public async store({ auth, request, response }: HttpContextContract) {
+    const user = await auth.authenticate();
+    const drink = new Drink();
+    drink.name = request.input('name');
+    drink.description = request.input('description');
+    await drink.save()
+    return drink
+  }
+
+  public async destroy({ response, auth, request, params }: HttpContextContract) {
+    const user = await auth.authenticate();
+    const drink = await Drink.query().where('id', params.id).delete();
+    return response.json({ message: "Deleted successfully" })
+  }
+}
---
+++ app/Models/Drink.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Category from './Category';
+
+export default class Drink extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string;
+
+  @column()
+  public description: string;
+
+  @column()
+  public categoriaId: number;
+
+  @belongsTo(() => Category)
+  public category: BelongsTo<typeof Category>;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
