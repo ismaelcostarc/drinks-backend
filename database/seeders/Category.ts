ee43fac433ca7c2d6b8c4687d6940a2f97557152
Ismael Costa <ismaelac96@icloud.com>
2023-12-21 00:19:03 -0300

    feat: populate the database

--- database/seeders/Category.ts
+++ database/seeders/Category.ts
@@ -2,20 +2,35 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Category from 'App/Models/Category'
 
 export default class extends BaseSeeder {
-  public async run () {
-    await Category.createMany([
-      {
-        name: 'Whisky',
-        description: 'O uísque é uma bebida rigorosamente regulamentada; possui denominações de origem e várias classes e tipos.',
-      },
-      {
-        name: 'Cerveja',
-        description: 'A cerveja é uma bebida produzida a partir da fermentação de cereais, principalmente a cevada maltada.'
-      },
-      {
-        name: 'Vodka',
-        description: 'Vodca é uma popular bebida destilada, incolor, quase sem sabor e com um teor alcoólico entre 35 e 60%.'
-      },
+  public async run() {
+    await Category.createMany([{
+      name: 'Whisky',
+      description: 'O uísque é uma bebida rigorosamente regulamentada; possui denominações de origem e várias classes e tipos.',
+    },
+    {
+      name: 'Cerveja',
+      description: 'A cerveja é uma bebida produzida a partir da fermentação de cereais, principalmente a cevada maltada.'
+    },
+    {
+      name: 'Vodka',
+      description: 'Vodca é uma popular bebida destilada, incolor, quase sem sabor e com um teor alcoólico entre 35 e 60%.'
+    },
+    {
+      name: 'Rum',
+      description: 'Rum é uma bebida alcoólica destilada de açúcar ou melaço de cana-de-açúcar fermentado.'
+    },
+    {
+      name: 'Tequila',
+      description: 'A tequila é uma bebida destilada do México, feita da agave-azul.'
+    },
+    {
+      name: 'Gin',
+      description: 'Gin é uma bebida destilada à base de cereais e aromatizada com zimbro e outros botânicos.'
+    },
+    {
+      name: 'Licor',
+      description: 'Licor é uma bebida alcoólica adocicada, geralmente com sabor de frutas, ervas ou especiarias.'
+    },
     ])
   }
 }
--- database/seeders/Drink.ts
+++ database/seeders/Drink.ts
@@ -2,7 +2,7 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Drink from 'App/Models/Drink'
 
 export default class extends BaseSeeder {
-  public async run () {
+  public async run() {
     await Drink.createMany([
       {
         categoryId: 1,
@@ -12,16 +12,83 @@ export default class extends BaseSeeder {
       },
       {
         categoryId: 2,
-        name: 'Bananinha',
-        description: '...',
-        imgUrl: 'https://bebidasdomundo.com/wp-content/uploads/2020/11/6155888981_1.jpg',
+        name: 'Budweiser',
+        description: 'Budweiser é uma cerveja americana produzida pela Anheuser-Busch.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/1510-cerveja-budweiser-lata-350ml.jpg?s=27481d070256cfbebcd7860b8d9b4004',
       },
       {
         categoryId: 3,
-        name: 'Slova',
-        description: '...',
-        imgUrl: 'https://a-static.mlcdn.com.br/450x450/vodka-slova-garrafa-970ml/bayeuxdepositoaltograu/54fd2528f8a611ec84484201ac185079/a1f0a3e4f927567bf4eadc1fac362919.jpg',
+        name: 'Absolut Vodka',
+        description: 'Absolut Vodka é uma marca sueca de vodka produzida em Åhus, no sul da Suécia.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/10401-vodka-absolut-azul-1l.jpg?s=27481d070256cfbebcd7860b8d9b4004',
       },
-    ])
+      {
+        categoryId: 4,
+        name: 'Havana Club',
+        description: 'Havana Club é uma marca de rum produzida em Santa Cruz del Norte, Cuba.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/8244-rum-havana-club-anejo-7-anos-1l.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 5,
+        name: 'Patrón',
+        description: 'Patrón é uma marca de tequila produzida no México.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/10919-tequila-patron-silver-750ml.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 6,
+        name: 'Bombay Sapphire',
+        description: 'Bombay Sapphire é uma marca de gin produzida pela Bacardi.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/7213-gin-bombay-sapphire-1l.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 7,
+        name: 'Baileys',
+        description: 'Baileys é um licor irlandês à base de whisky e creme de leite.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/7266-licor-baileys-irish-cream-1l.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 4,
+        name: 'Appleton Estate',
+        description: 'Appleton Estate é uma marca de rum jamaicana.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/20901-rum-appleton-estate-8-anos-1l.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 2,
+        name: 'Stella Artois',
+        description: 'Stella Artois é uma cerveja lager belga, originária da cidade de Leuven.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/1027-cerveja-stella-artois-long-neck-330ml.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 3,
+        name: 'Cîroc Vodka',
+        description: 'Cîroc é uma marca francesa de vodka produzida a partir de uvas.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/16547-vodka-ciroc-red-berry-750ml.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 1,
+        name: 'Jameson',
+        description: 'Jameson é uma marca de uísque irlandês produzida pela Irish Distillers.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/6336-whisky-jameson-1l.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 5,
+        name: 'José Cuervo',
+        description: 'José Cuervo é uma marca de tequila mexicana.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/10063-tequila-jose-cuervo-especial-gold-750ml.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 6,
+        name: 'Beefeater',
+        description: 'Beefeater é uma marca de gin produzida em Londres.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/10136-gin-beefeater-londres-dry-750ml.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+      {
+        categoryId: 7,
+        name: 'Kahlúa',
+        description: 'Kahlúa é um licor de café originário do México.',
+        imgUrl: 'https://www.imigrantesbebidas.com.br/img/bebida/images/products/full/7258-licor-kahlua-750ml.jpg?s=27481d070256cfbebcd7860b8d9b4004',
+      },
+    ]
+    )
   }
 }
